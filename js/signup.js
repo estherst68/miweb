cb23e781e0c0b15867ca2aa7afcfd18d884c3651
esther <estherst.6368@gamil.com>
2023-09-26 20:20:01 +0200

    202309262019

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,5 @@
-const signupForm = document.querySelector('#signupForm')
-signupForm.addEventListener('submit', (e)=>{
+const signupform = document.querySelector('#signupForm')
+signupform.addEventListener('submit', (e)=>{
     e.preventDefault()
     const name = document.querySelector('#name').value
     const email = document.querySelector('#email').value
